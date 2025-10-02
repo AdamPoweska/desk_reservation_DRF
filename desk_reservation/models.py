335d89602344a9eed72215e1161e6429192d44e9
Adam Poweska <adam.poweska@gmail.com>
2025-10-02 23:31:59 +0200

    save

--- desk_reservation/models.py
+++ desk_reservation/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Floor(models.Model):
+    floor_number = models.IntegerField(max_value=10, min_value=1)
+
+class Desk(models.Model):
+    desk_number = models.IntegerField(max_value=100, min_value=1)
+    floor = models.ForeignKey(Floor)
+    reservation = models.BooleanField()
+
+    class Meta:
+        def __repr__(self):
+            return f"{self.floor}-{self.desk_number}"
