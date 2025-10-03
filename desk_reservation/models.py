2a8b1c65a67682f29bf590f46964e7761599336f
Adam Poweska <adam.poweska@gmail.com>
2025-10-03 23:27:51 +0200

    small update on models

--- desk_reservation/models.py
+++ desk_reservation/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class Floor(models.Model):
     floor_number = models.IntegerField(max_value=10, min_value=1)
 
+
 class Desk(models.Model):
+    floor = models.ForeignKey(Floor, on_delete=models.CASCADE)
     desk_number = models.IntegerField(max_value=100, min_value=1)
-    floor = models.ForeignKey(Floor)
     reservation = models.BooleanField()
 
     class Meta:
