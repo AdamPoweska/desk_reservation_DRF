f865275f54ee6161f58259b28ecd477c405eeaec
Adam Poweska <adam.poweska@gmail.com>
2025-10-16 23:04:23 +0200

    Worker model added

--- desk_reservation/models.py
+++ desk_reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class Floor(models.Model):
     floor_number = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
 
@@ -13,3 +14,13 @@ class Desk(models.Model):
     class Meta:
         def __repr__(self):
             return f"{self.floor}-{self.desk_number}"
+        
+
+class Worker(models.Model):
+    name = models.IntegerField(max_length=30)
+    surname = models.IntegerField(max_length=30)
+    id_number = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        def __repr__(self):
+            return f"{self.name}{self.surname}-{self.id_number}"
--- desk_reservation/urls.py
+++ desk_reservation/urls.py
@@ -6,7 +6,6 @@ router = DefaultRouter()
 router.register(f'desk', views.DeskViewSet, basename='desk')
 router.register(f'floor', views.FloorViewSet, basename='floor')
 router.register(f'users', views.UserViewSet, basename='user')
-# router.register(f'api-auth', include('rest_framework.urls'), basename='api-auth')
 
 urlpatterns = [
     path('', include(router.urls)),
