a21c5a1e1e4c66a8dc18f4b8ecc201147035ec3a
Adam Poweska <adam.poweska@gmail.com>
2025-11-02 22:18:44 +0100

    after user creation password will be seen only once

--- desk_reservation/serializers.py
+++ desk_reservation/serializers.py
@@ -30,11 +30,18 @@ class WorkerSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
     
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
+        validated_data.pop('password', None) # .pop on variable itself will remove it fully from dictionary, if we would use "validated_data.pop('password', None)" - we could still use password given by the admin
         password = User.objects.make_random_password()
         user = User.objects.create_user(password=password, **validated_data)
         
+        user._generated_password = password # w pythonie możemy dodać atrybut dynamicznie do obiektu (składnia pythona a nie django/drf). "_generated_password" nie zostanie nigdzie zapisane - bo nie ma takiego pola w serializerze. Jednak będzie działać jeszcze z return i dlatego będzie można je zobaczyć jeden raz - i tylko ten jeden raz.
         return user
+    
+    def to_representation(self, instance): # "To implement a custom relational field, you should override RelatedField, and implement the .to_representation(self, value) method."
+        data = super().to_representation(instance) # super() tworzy klasę która dziedziczy wszystkie metody i właściwości innej klasy -> w tym przypadku super() jest wywoływane na funkcji a nie klasie. W takim wypadku super() wywołuje METODĘ O TEJ NAZWIE Z KLASY NADRZĘDNEJ
+        if hasattr(instance, '_generated_password'): # jeżeli instance ma _generated_password
+            data['generated_password'] = instance._generated_password # to dodajemy klucz "generated_password" do słownika DATA i nadajemy mu wartość która była przekazana do funkcji jako INSTANCE
+        return data
 
 """
 def create(self, validated_data):
